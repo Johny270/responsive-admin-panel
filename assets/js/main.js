3cdc49506c66a505d2a8b0b5dddcc66d6db4f8dd
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-23 15:09:01 -0400

    Implement toggle functionality to skrink and expand the navigation menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,14 @@ function activeLink() {
   this.classList.add('hovered');
 }
 
-list.forEach(item => item.addEventListener("mouveover", activeLink))
+list.forEach(item => item.addEventListener("mouseover", activeLink));
+
+// Menu toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function() {
+  navigation.classList.toggle("active");
+  main.classList.toggle('active');
+}
